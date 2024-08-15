513a2e8079d0b7db506a745e4bfa7fbdff9922ce
vais55 <vais55.va@gmail.com>
2024-08-15 20:39:59 +0300

    It didn’t work with curseforge (Cloudflare protection), I’m trying modrinth parsing

--- mod_loader.py
+++ mod_loader.py
@@ -1,5 +1,16 @@
-import curseforge
+# import curseforge
+#
+# client = curseforge.CurseClient()
+#
+# minecraft = client.game(432)
+# для curseforge
 
-client = curseforge.CurseClient()
+import requests
+from bs4 import BeautifulSoup
 
-minecraft = client.game(432)
+modrinth = requests.get('https://modrinth.com/mod/create')
+print(modrinth.status_code)
+
+modrinth_source = modrinth.text
+
+modrinth_soup = BeautifulSoup(modrinth_source, 'html.parser')
