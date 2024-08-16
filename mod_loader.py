6386ddfce117194e9777c17082a0e2575b2e9925
vais55 <vais55.va@gmail.com>
2024-08-16 18:26:23 +0300

    working with Modrinth API instead of parsing Modrinth.com

--- mod_loader.py
+++ mod_loader.py
@@ -1,33 +1,15 @@
+import modrinth
 import requests
-from bs4 import BeautifulSoup
+import os
 
-mod = 'TerraFirmaCraft'
-minecraft_version = '1.12.2'
+mod_name = 'NEI'
+mod = modrinth.Projects.ModrinthProject(mod_name)
+mod_name = mod.name
+mod_id = mod.id
+mod_version = mod.getVersion(mod.versions[-1])
+mod_link = mod_version.getDownload(mod_version.getFiles()[0])
 
-modrinth = requests.get(f'https://modrinth.com/mod/{mod.lower()}/versions?g={minecraft_version}')
-
-modrinth_soup = BeautifulSoup(modrinth.text, 'html.parser')
-
-mod_div = modrinth_soup.find('div', class_='version-button button-transparent')
-mod_div_text = mod_div.get_text()
-
-mod_version = modrinth_soup.find('div', class_='version__metadata')
-mod_version_text = mod_version.get_text().replace(' Release', '')
-
-mod_link = f'https://modrinth.com/mod/{mod.lower()}/version/{mod_version_text}'
-
-mod_page = requests.get(mod_link)
-print(mod_page.status_code)
-
-mod_page_soup = BeautifulSoup(mod_page.text, 'html.parser')
-
-mod_page_div = mod_page_soup.find('div',
-                                  class_='version-page__title universal-card').find('div', class_='input-group')
-
-mod_download_link = mod_page_div.find('a')['href']
-
-mod_requirements = mod_page_soup.find('div', class_='version-page__dependencies universal-card')
-
-mod_requirement = mod_requirements.find('div', class_='button-transparent dependency')
-
-print(mod_requirements)
+mod_file = requests.get(mod_link)
+mod_file_name = mod_version.name + '.jar'
+with open(f'{mod_file_name}', 'wb') as file:
+    file.write(mod_file.content)
