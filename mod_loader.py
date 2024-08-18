e79e4e50cb84d42fa4541f34d51f92b01a0e2bd9
vais55 <vais55.va@gmail.com>
2024-08-18 12:36:54 +0300

    Mod loader is completely ready, started work on visual

--- mod_loader.py
+++ mod_loader.py
@@ -2,44 +2,91 @@ import requests
 import json
 import os
 
-project_name = input('Введи название проекта: ')
-project_id = ''
-game_version = input('Введи версию игры: ')
-loader = input('Введи название загрузчика модов(Forge, Fabric, и т. д.): ').lower()
-search = requests.get('https://api.modrinth.com/v2/search', params={'query': project_name,
+
+def check_directory():
+    if not os.path.isdir('mods'):
+        os.mkdir('mods')
+        os.chdir('mods')
+    else:
+        os.chdir('mods')
+
+
+def get_project_files(project_id, download_optional_dependencies_flag, game_version):
+    project = requests.get(f'https://api.modrinth.com/v2/project/{project_id}').text
+    project_json = json.loads(project)
+    project_versions = project_json['versions']
+    project_versions_codes = []
+    project_versions_numbers = []
+    for project_version_code in project_versions:
+        version = requests.get(f'https://api.modrinth.com/v2/project/{project_id}/version/{project_version_code}').text
+        version_json = json.loads(version)
+        if game_version in version_json['game_versions']:
+            project_versions_codes.append(project_version_code)
+            project_version = version_json['version_number']
+            project_versions_numbers.append(project_version)
+    project_versions = dict(zip(project_versions_numbers, project_versions_codes))
+    print(*(list(project_versions.keys())))
+    desired_project_version = input('Enter the desired version from list above: ')
+    version = requests.get(
+        f'https://api.modrinth.com/v2/project/{project_id}/version/{project_versions[desired_project_version]}').text
+    version_json = json.loads(version)
+    project_dependencies = version_json['dependencies']
+    project_file_link = version_json['files'][0]['url']
+    project_file_name = version_json['files'][0]['filename']
+    project_file = requests.get(project_file_link).content
+    download_project_files(project_file, project_file_name)
+    if len(project_dependencies) > 0:
+        download_dependencies(project_dependencies, download_optional_dependencies_flag, game_version)
+
+
+def download_project_files(project_file, project_file_name):
+    with open(f'{project_file_name}', 'wb') as file:
+        file.write(project_file)
+    print(f'Downloaded and installed {project_file_name}')
+
+
+def download_dependencies(project_dependencies, download_optional_dependencies_flag, game_version):
+    for dependency in project_dependencies:
+        match dependency['dependency_type']:
+            case 'required':
+                get_project_files(dependency['project_id'], download_optional_dependencies_flag, game_version)
+            case 'optional':
+                if download_optional_dependencies_flag:
+                    get_project_files(dependency['project_id'], download_optional_dependencies_flag, game_version)
+                else:
+                    print(f'Skipped installing {dependency['project_id']}')
+
+
+def search_project(project_name, loader, game_version, download_optional_dependencies_flag):
+    project_id = ''
+    search = requests.get('https://api.modrinth.com/v2/search', params={'query': project_name,
                                                                         'categories': loader,
                                                                         'versions': game_version}).text
-search_json = json.loads(search)['hits']
-for i in range(len(search_json)):
-    if search_json[i]['title'] == project_name:
-        project_id = search_json[i]['project_id']
-        break
+    search_json = json.loads(search)['hits']
+    for search_result in range(len(search_json)):
+        if search_json[search_result]['title'] == project_name:
+            project_id = search_json[search_result]['project_id']
+            break
 
-if project_id == '':
-    print(f'Project {project_name} not found.')
-    exit(1)
-project = requests.get(f'https://api.modrinth.com/v2/project/{project_id}').text
-project_json = json.loads(project)
-project_versions = project_json['versions']
-mod_versions_codes = []
-mod_versions_numbers = []
-for i in project_versions:
-    version = requests.get(f'https://api.modrinth.com/v2/project/{project_id}/version/{i}').text
-    version_json = json.loads(version)
-    if game_version in version_json['game_versions']:
-        mod_versions_codes.append(i)
-        mod_version = version_json['version_number']
-        mod_versions_numbers.append(mod_version)
-mod_versions = dict(zip(mod_versions_numbers, mod_versions_codes))
-print(*(list(mod_versions.keys())))
-requred_mod_version = input('Выбери(напиши в консоль) нужную тебе версию мода из списка выше: ')
-version = requests.get(f'https://api.modrinth.com/v2/project/{project_id}/version/{mod_versions[requred_mod_version]}').text
-version_json = json.loads(version)
-mod_file_link = version_json['files'][0]['url']
-mod_file_name = version_json['files'][0]['filename']
-mod_file = requests.get(mod_file_link).content
-if not os.path.isdir('mods'):
-    os.makedirs('mods')
-with open(f'{mod_file_name}', 'wb') as file:
-    file.write(mod_file)
-os.replace(f'{mod_file_name}', f'mods/{mod_file_name}')
+    if project_id == '':
+        print(f'Project {project_name} not found.')
+        exit(1)
+
+    get_project_files(project_id, download_optional_dependencies_flag, game_version)
+
+
+def main():
+    check_directory()
+    project_name = input('Enter the project name: ')
+    game_version = input('Enter the game version: ')
+    download_optional_dependencies_flag = input('Do you want to download optional dependencies? (y/n): ')
+    if download_optional_dependencies_flag == 'y':
+        download_optional_dependencies_flag = True
+    else:
+        download_optional_dependencies_flag = False
+    loader = input('Enter the loader (Forge, Fabric, etc): ').lower()
+    search_project(project_name, loader, game_version, download_optional_dependencies_flag)
+
+
+if __name__ == '__main__':
+    main()
