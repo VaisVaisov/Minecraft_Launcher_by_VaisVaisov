56468505eaca59723a786037ddcc608ef68a21f3
vais55 <vais55.va@gmail.com>
2025-01-15 22:36:51 +0500

    some problems with forge installing

--- main.py
+++ main.py
@@ -12,10 +12,19 @@ options = {
 }
 
 
+def forge_install():
+    forge_version = minecraft_launcher_lib.forge.find_forge_version(vanilla_version=version)
+    minecraft_launcher_lib.forge.forge_to_installed_version(forge_version=forge_version)
+
+
 def launch():
     minecraft_launcher_lib.install.install_minecraft_version(versionid=version, minecraft_directory=directory)
 
     if input('Would you like to install Forge? (y/n): ') == 'y':
-        minecraft_launcher_lib.forge.forge_to_installed_version(forge_version=version)
+        forge_install()
 
     subprocess.call(minecraft_launcher_lib.command.get_minecraft_command(version=version, minecraft_directory=directory, options=options))
+
+if __name__ == '__main__':
+    launch()
+    
